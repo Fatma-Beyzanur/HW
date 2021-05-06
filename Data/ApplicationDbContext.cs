372f6364c19dcd2eaaf97ceed9207a2381d50161
Fatma-Beyzanur <Bozel123>
2021-05-06 11:32:02 +0300

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using HW.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<student> Students { get; set; }
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HW.Models
+{
+    public class student
+    {
+        public student()
+        {
+            CreationDate = DateTime.Today;
+            LastUpdateDate = DateTime.Now;
+        }
+
+        public int Id { get; set; }
+        [DisplayName("Student ID")]
+        [Required]
+        public string StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        public string Department { get; set; }
+        [Required]
+        public DateTime Birthdate { get; set; }
+        public int Height { get; set; }
+        public int Weight { get; set; }
+
+        [DisplayName("Creation Date")]
+        public DateTime CreationDate { get; set; }
+        [DisplayName("Last Update Date")]
+
+        public DateTime LastUpdateDate { get; set; }
+
+
+    }
+}
